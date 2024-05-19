c084dad73321c55d03cc274a558b58193a82d126
priyanshu bharti <110742345+priyanshub3443@users.noreply.github.com>
2024-05-19 13:00:46 +0530

    day 1

---
+++ day 1/filleq.py
@@ -0,0 +1,6 @@
+string = "15"
+number = 7
+string_number = int(
+    string)  #throws an error if the string is not a valid integer
+sum = number + string_number
+print("The Sum of both the numbers is: ", sum)
