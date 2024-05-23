bf70aadf8b5abd05e645e057a5f5aa01ddea7a1f
priyanshu bharti <110742345+priyanshub3443@users.noreply.github.com>
2024-05-23 17:05:53 +0530

    day 4

---
+++ day 4/prog5.py
@@ -0,0 +1,8 @@
+# cities = {"Tokyo", "Madrid", "Berlin", "Delhi"}
+# cities2 = {"Seoul", "Kabul", "Delhi"}
+# cities3 = cities.difference(cities2)
+# print(cities3)
+cities = {"Tokyo", "Madrid", "Berlin", "Delhi"}
+cities2 = {"Tokyo", "Seoul", "Kabul", "Madrid"}
+cities3 = cities.intersection(cities2)
+print(cities3)
